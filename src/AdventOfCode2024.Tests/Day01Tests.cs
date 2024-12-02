553daebe46c7fcd6cb2b869104b2b7cad147196a
Josh Brunton <josh.brunton@proton.me>
2024-12-02 10:23:27 +0000

    Day 1

---
+++ src/AdventOfCode2024.Tests/Day01Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2024.Tests;
+
+[TestClass]
+public sealed class Day01Tests
+{
+    [TestMethod]
+    public void TestDay1Part1()
+    {
+        Assert.AreEqual(Day01.Day01.Part1(), 1189304);
+    }
+
+    [TestMethod]
+    public void TestDay1Part2()
+    {
+        Assert.AreEqual(Day01.Day01.Part2(), 24349736);
+    }
+}
---
+++ src/AdventOfCode2024/Day01/Day01.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode2024.Day01
+{
+    public class Day01
+    {
+        private static IEnumerable<string> GetInput()
+        {
+            string path = Path.Join(".", "Day01", "input.txt");
+            return File.ReadLines(path);
+        }
+
+        public static int Part1()
+        {
+            IEnumerable<string> input = GetInput();
+
+            // split the input into 2 lists 
+            List<int> left = [];
+            List<int> right = [];
+            foreach (string line in input)
+            {
+                Console.WriteLine();
+                left.Add(int.Parse(line.Split("   ")[0]));
+                right.Add(int.Parse(line.Split("   ")[1]));
+            }
+
+            // order the lists in place 
+            left.Sort();
+            right.Sort();
+
+            // find the diffs 
+            List<int> diffs = [];
+            for (int i = 0; i < left.Count; i++)
+            {
+                diffs.Add(Math.Abs(left[i] - right[i]));
+            }
+
+            // sum them
+            int ret = diffs.Sum();
+            Console.WriteLine(ret);
+
+            return ret;
+        }
+
+        public static int Part2()
+        {
+            IEnumerable<string> input = GetInput();
+
+            List<int> left = [];
+            List<int> right = [];
+            foreach (string line in input)
+            {
+                Console.WriteLine();
+                left.Add(int.Parse(line.Split("   ")[0]));
+                right.Add(int.Parse(line.Split("   ")[1]));
+            }
+
+            left = left.Distinct().ToList();
+            List<int> similarityScores = [];
+            foreach (int i in left)
+            {
+                similarityScores.Add(i * right.Count(x => x == i));
+            }
+
+            return similarityScores.Sum();
+        }
+    }
+}
